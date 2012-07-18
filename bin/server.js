d9784cc82059102ee644bf55bfd9f97e0284ebf3
Ryan Cole <ryan@rycole.com>
2012-07-18 16:08:31 -0500

    make note of env

--- bin/server.js
+++ bin/server.js
@@ -4,6 +4,6 @@ var server = require('../src/server');
 // start the web server
 server.listen(80, function () {
     
-    console.log('web server listening');
+    console.log(server.settings.env + ' web server listening');
     
 });
